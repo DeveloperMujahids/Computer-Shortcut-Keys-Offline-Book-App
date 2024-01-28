bdfe58ac4dd2b6d5ad8077d0d2e468209685f00e
DeveloperMujahids <mujahid247866@gmail.com>
2024-01-27 20:48:03 -0800

    testing

--- app/src/main/java/com/itsmujahid/computershortcutspro/MainActivity.java
+++ app/src/main/java/com/itsmujahid/computershortcutspro/MainActivity.java
@@ -3,13 +3,117 @@ package com.itsmujahid.computershortcutspro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+
+
+
+
+
+        ////-----------------------------------------Bottom Navigation Code Start Here-----------------------------------
+
+        bottomNavigation.show(2, true);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.internet_store));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.baseline_paid_24));
+
+        bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+                switch (model.getId()) {
+                    //////////////////////////////
+                    case 1:
+
+
+
+                        break;
+///////////////////////////////
+                    case 2:
+
+
+
+                        break;
+////////////////////////////////
+                    case 3:
+
+
+
+                        break;
+                    //////////////////////////////
+                }
+
+                return null;
+            }
+        });
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+                switch (model.getId()) {
+                    case 1:
+
+                        break;
+                }
+                return null;
+            }
+        });
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+                switch (model.getId()) {
+                    case 2:
+
+                        break;
+                }
+                return null;
+            }
+        });
+        bottomNavigation.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                // YOUR CODES
+                switch (model.getId()) {
+                    case 3:
+
+                        break;
+                }
+                return null;
+            }
+        });
+
+        ////-----------------------------------------Bottom Navigation Code End Here-----------------------------------
+
+
+
+
+
+
+
+
+
+
+
 
 
 
